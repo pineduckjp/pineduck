ec93f4a402dc32cccfbe720dffc9b18341241bc9
PineDuck <info@hujbvf.com>
2026-07-24 15:14:53 +0900

    feat: セレクトレイアウトページの追加 (#10)
    * doc: PR テンプレートの更新
    
    - マージ対象ブランチ選択フォーマットで、チェックボックスではなく「◉」「○」を使用するように変更
    - 不要なコメントを削除
    
    * feat: ホーム画面の追加
    
    - Headコンポーネントの追加
      - ファビコン、ウェブマニフェスト自動生成
      - ローカルフォント導入
    - Headerコンポーネントの追加
    - Footerコンポーネントの追加
    - StaticSpriteコンポーネントの追加
      - シンボルスプライトの追加
      - ロゴアイコンスプライトの追加
    - DynamicSpriteの追加
      - canvasスプライトの追加
    - Selectレイアウトの追加
    - ホームページの追加
    
    Issue #3
    
    * style: アセットの更新
    
    - PineDuckダイアリー関連のアセットを追加
    - シンボルを複数追加・変更
    
    Issue #3
    
    * fix: ホーム画面の修正
    
    - スクロール範囲を変更
    - StaticSpriteで名前の冒頭が被ると上手く表示できない問題を修正
    
    Issue #3
    
    * feat: PineDuckカフェ画面の追加
    
    Issue #3
    
    * feat: PineDuckアート画面の追加
    
    Issue #3
    
    * feat: PineDuckファーム画面の追加
    
    Issue #3
    
    * feat: PineDuckクッキング画面の追加
    
    Issue #3
    
    * feat: PineDuckソーイング画面の追加
    
    Issue #3
    
    * feat: PineDuckストーリー画面の追加
    
    Issue #3
    
    * feat: PineDuckビルド画面の追加
    
    Issue #3
    
    * feat: PineDuckキャンプ画面の追加
    
    Issue #3
    
    * feat: PineDuckダイアリー画面の追加
    
    Issue #3
    
    * feat: アヒルのしっぽ画面の追加
    
    Issue #3
    
    * feat: アヒルのポートフォリオ画面の追加
    
    Issue #3
    
    * feat: アヒルのスターター画面の追加
    
    Issue #3
    
    * feat: アヒルのエディター画面の追加
    
    Issue #3
    
    * style: SelectListの属性の順番を揃える
    
    Issue #3
    
    * docs: README の構成を更新
    
    Issue #3
    
    * docs: スプライトシートの扱いに関するドキュメントの追加
    
    Issue #3
    
    ---------
    
    Co-authored-by: pineduckjp <hello@pineduck.jp>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,45 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
+
+import favicons from "astro-favicons";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://pineduck.jp/",
+  devToolbar: {
+    enabled: false,
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Dot",
+      cssVariable: "--font-dot",
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/JF-Dot-Shinonome-16-Subset.woff2"],
+            display: "block",
+          },
+        ],
+      },
+    },
+  ],
+  integrations: [
+    favicons({
+      name: "PineDuck",
+      short_name: "PineDuck",
+      background: "rgb(229 228 227)",
+      themes: ["rgb(229 228 227)"],
+      manifest: {
+        description: "自給自足プラットフォーム「PineDuck」のホームページです。",
+        id: "/",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        display_override: ["standalone", "browser"],
+      },
+    }),
+  ],
+});
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "PineDuck";
+export const SITE_DESCRIPTION =
+  "自給自足プラットフォーム「PineDuck」のホームページです。";
+
+export const PINEDUCK_URL = "https://pineduck.jp/";
